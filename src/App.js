502ab768826a5bd9d48eee3767e6798bc6e574cc
Okulova Valya <valuhaokulova@gmail.com>
2021-10-12 18:36:16 +0700

    trying to delete by dnd

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import './App.css';
 import * as React from 'react';
 import Table from './components/table/table';
 import Button from '@mui/material/Button';
+import redBin from './icons/red bin.png'
+
 const axios = require('axios').default;
 
 function App() {
   const [games, setGames] = React.useState(null);
+  const [deletingGame, setDeletingGame] = React.useState(null);
   const URL = 'https://api.npoint.io/35e19105588b7552df5d';
+  const [dragOver, setDragOver] = React.useState(false);
 
   function load(){
     axios.get(URL)
@@ -21,17 +25,51 @@ function App() {
     })
   };
 
+  function deleteGame(e){
+    setGames(games.splice(deletingGame.id, 1));
+    console.log(games)
+    setDeletingGame(null);
+  }
+
+  function deleteDragOver(e){
+    e.preventDefault();
+    e.target.style.background = "#FFCDD2";
+    e.target.style.border = "3px dashed #EF5350";
+    setDragOver(true)
+  }
+
+  function deleteDragEnd(e){
+    setDragOver(false);
+    setDeletingGame(null);
+  }
+
+  function deleteDragLeave(e){
+    setDragOver(false);
+    e.target.style.background = "#B2EBF2";
+    e.target.style.border = "3px dashed #26C6DA";
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <p> GamesLib </p>
       </header>
-      { games ? <Table data={games}/> :
+      { deletingGame &&  
+        <div className="deleteDrop" 
+        onDrop={(e) => deleteGame(e)} 
+        onDragOver={(e) => deleteDragOver(e)} 
+        onDragLeave={(e) => deleteDragLeave(e)} 
+        onDragEnd={(e) => deleteDragEnd(e)}
+        > 
+          { dragOver ? <img src={redBin} alt="red trash" className="redBin"/> : <span> Переместите сюда игру чтобы удалить </span>}
+        </div>
+      }
+      { games ? <Table key={games} data={games} setDeletingGame={setDeletingGame} setData={setGames} currentGame={deletingGame} setCurrentGame={setDeletingGame}/> :
         <div className="noList-main">
           <p className="noList-text"> У вас еще нет добавленных игр. Нажмите 'Загрузить', чтобы загрузить игры с URL, 
             <br/> или нажмите 'Добавить', чтобы добавить новую игру в ваш список!</p>        
           <div className="noList-buttons">
-            <Button variant="contained" onClick={load} className="noList-LoadButton">
+            <Button size="large" variant="contained" onClick={load} className="noList-LoadButton">
               Загрузить
             </Button>
           </div>
--- src/components/table/table.js
+++ src/components/table/table.js
@@ -5,20 +5,17 @@ import ImageListItemBar from '@mui/material/ImageListItemBar';
 import './table.css';
 
 export default function Table(props) {
-  const [games, setGames] = React.useState(props.data);  
-  const [currentGame, setCurrrentGame] = React.useState(null);
-
   function imgClick(url){
-    console.log(url);
     window.open(url);
   }
 
   function dragStart(e, item){
-    setCurrrentGame(item);
+    e.stopPropagation()
+    props.setDeletingGame(item);
   }
 
   function dragEnd(e){  
-    e.target.style.opacity = "1";    
+    e.target.style.opacity = "1";   
   }
 
   function dragOver(e){
@@ -28,32 +25,32 @@ export default function Table(props) {
   
   function drop(e, item){    
     e.preventDefault();
-
-    setGames(games.map(c => {
+    props.setData(props.data.map(c => {
         if (c.id === item.id){
-            return {...c, pos: currentGame.pos}
+            return {...c, pos: props.currentGame.pos}
         }
-        if (c.id === currentGame.id){
+        if (c.id === props.currentGame.id){
             return {...c, pos: item.pos}
         }
         return c 
     }))
 
+    props.setDeletingGame(null); 
     e.target.style.opacity = "1";   
   }
 
   const sortGames = (a,b) => {
-      if (a.pos > b.pos){
+    if (a.pos > b.pos){
         return 1
-      }
-      return -1
+    }
+    return -1
   }
 
   return (
     <>
-        { games && <ImageList cols={4} gap={10}> 
-            { games.sort(sortGames).map((item) => (
-                <ImageListItem key={item.id} onClick={() => imgClick(item.icon_url)} 
+        { props.data && <ImageList cols={4} gap={10}> 
+            { props.data.map((item, index) => (
+                <ImageListItem key={index} onClick={() => imgClick(item.icon_url)} 
                 draggable={true}
                 onDragStart={(e) => dragStart(e,item)}
                 onDragLeave={(e) => dragEnd(e)}
